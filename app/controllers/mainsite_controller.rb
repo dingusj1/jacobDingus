b88ef685176bd501c1881d9d15df3270994e880f
Jacob Dingus <jacob.dingus@gmail.com>
2012-01-16 04:30:09 -0500

    email and contact info

--- app/controllers/mainsite_controller.rb
+++ app/controllers/mainsite_controller.rb
@@ -4,4 +4,12 @@ class MainsiteController < ApplicationController
 
   def webume
   end
+  
+  def contactPage
+  end
+
+  def send_mail
+    Email_Me::contact_email(params[:email]).deliver
+    redirect_to mainsite_webume_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 JacobDingus::Application.routes.draw do
   get "mainsite/index"
   get "mainsite/webume"
+  get "mainsite/contactPage"
+  post "mainsite/send_mail"
   
   root :to => 'mainsite#index'
 
